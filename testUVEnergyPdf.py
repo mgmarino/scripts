cbd4b98d045a7f9308d0571a41fabaad4acb63d0
Wolfhart Feldmeier <wolfhart.feldmeier@mytum.de>
2012-09-13 14:18:17 -0700

    Added script testUVEnergyPdf.py

---
+++ testUVEnergyPdf.py
@@ -0,0 +1,20 @@
+import ROOT,math
+
+def main():
+  ROOT.gSystem.Load("libEXOROOT")
+  cluster = ROOT.EXOClusteringModule()
+  nbins = 200
+  upperU = 4000
+  upperV = 1000
+  hist = ROOT.TH2D("hist","hist",nbins,0,upperU,nbins,0,upperV)
+  for j,v in enumerate(range(0,upperV,upperV/nbins)):
+    for i,u in enumerate(range(0,upperU,upperU/nbins)):
+      val = math.exp(-cluster.energyNLPdf(u,v))
+      #val = -cluster.energyNLPdf(u,v)
+      hist.SetBinContent(i,j,val)
+  c1 = ROOT.TCanvas()
+  hist.Draw("colz")
+  raw_input("hit enter to quit")
+
+if __name__ == "__main__":
+  main()
