68972f57c37ab32cea6f44385575be9dfb32b22e
Wolfhart Feldmeier <wolfhart.feldmeier@mytum.de>
2012-09-13 17:02:15 -0700

    changes in testUVEnergyPdf.py

--- test/testUVEnergyPdf.py
+++ test/testUVEnergyPdf.py
@@ -4,8 +4,8 @@ def main():
   ROOT.gSystem.Load("libEXOROOT")
   cluster = ROOT.EXOClusteringModule()
   nbins = 200
-  upperU = 4000
-  upperV = 1000
+  upperU = 2000
+  upperV = 500
   hist = ROOT.TH2D("hist","hist",nbins,0,upperU,nbins,0,upperV)
   for j,v in enumerate(range(0,upperV,upperV/nbins)):
     for i,u in enumerate(range(0,upperU,upperU/nbins)):
