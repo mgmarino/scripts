f14470a7d15fa78a8830a9a7ea349f3cedb81044
Wolfhart Feldmeier <wolfhart.feldmeier@mytum.de>
2012-09-15 16:25:19 -0700

    improved Digitize.py: More settings can be made.

--- Digitize.py
+++ Digitize.py
@@ -1,4 +1,4 @@
-import ROOT,sys,random,os,objgraph
+import ROOT,sys,random,os
 from PyQt4 import QtGui, QtCore
 libpath = os.path.abspath('common')
 sys.path.append(libpath)
@@ -20,12 +20,24 @@ class Digitization(QtCore.QThread):
   eventProcessed = QtCore.pyqtSignal(int)
   neventsChanged = QtCore.pyqtSignal(int)
   nsignalsChanged = QtCore.pyqtSignal(int)
+  minenergyChanged = QtCore.pyqtSignal(float)
+  maxenergyChanged = QtCore.pyqtSignal(float)
+  maxdtChanged = QtCore.pyqtSignal(float)
+  minzChanged = QtCore.pyqtSignal(float)
+  maxzChanged = QtCore.pyqtSignal(float)
+  maxrChanged = QtCore.pyqtSignal(float)
     
   def __init__(self):
     super(Digitization,self).__init__()
 
     self.nevents = 100
     self.nsignals = 1
+    self.minEnergy = 100.
+    self.maxEnergy = 1000.
+    self.maxDt = 10.
+    self.minZ = -160.
+    self.maxZ = 160.
+    self.maxR = 160.
     self.abort = False
     self.noiseFilename = ""
     self.useRealNoise = False
@@ -55,6 +67,30 @@ class Digitization(QtCore.QThread):
     self.nsignals = nsignals
     self.nsignalsChanged.emit(nsignals)
 
+  def setMinEnergy(self,mine):
+    self.minEnergy = mine
+    self.minenergyChanged.emit(mine)
+
+  def setMaxEnergy(self,maxe):
+    self.maxEnergy = maxe
+    self.maxenergyChanged.emit(maxe)
+
+  def setMaxDt(self,maxdt):
+    self.maxDt = maxdt
+    self.maxdtChanged.emit(maxdt)
+
+  def setMinZ(self,minz):
+    self.minZ = minz
+    self.minzChanged.emit(minz)
+
+  def setMaxZ(self,maxz):
+    self.maxZ = maxz
+    self.maxzChanged.emit(maxz)
+
+  def setMaxR(self,maxr):
+    self.maxR = maxr
+    self.maxrChanged.emit(maxr)
+
   def setFilename(self,filename):
     self.toutmodule.SetOutputFilename(str(filename))
 
@@ -72,11 +108,7 @@ class Digitization(QtCore.QThread):
     self.digimodule.Initialize()
     self.toutmodule.Initialize()
 
-    min_energy,max_energy = 0.,4000.
     meantime = 1024*1000
-    maxtimediff = 200*1000
-    min_z,max_z = -160., 160.
-    max_r = 160.
 
     traceFile = ROOT.TFile()
     traceTree = ROOT.TTree()
@@ -91,20 +123,19 @@ class Digitization(QtCore.QThread):
         traceTree.SetBranchAddress("ch"+str(i),waveforms[i])
 
     for i in range(self.nevents):
-      if i == 500:
-        objgraph.show_most_common_types(limit=20)
       if(self.useRealNoise):
         traceTree.GetEntry(i%ntraces)
       self.eventProcessed.emit(i+1)
       for j in range(self.nsignals):
-        energy = random.uniform(min_energy,max_energy)
-        time = meantime + random.uniform(-maxtimediff,maxtimediff)
-        x = max_r
-        y = max_r
-        while x**2 + y**2 > max_r**2:
-          x = random.uniform(0.,max_r)
-          y = random.uniform(0.,max_r)
-        z = random.uniform(min_z,max_z)
+        energy = random.uniform(self.minEnergy,self.maxEnergy)
+        maxtimeshift = self.maxDt*1000./2.
+        time = meantime + random.uniform(-maxtimeshift,maxtimeshift)
+        x = self.maxR
+        y = self.maxR
+        while x**2 + y**2 > self.maxR**2:
+          x = random.uniform(0.,self.maxR)
+          y = random.uniform(0.,self.maxR)
+        z = random.uniform(self.minZ,self.maxZ)
         for gang in range(2*ROOT.NUMBER_APD_CHANNELS_PER_PLANE):
           self.pixel.AddAPDHit(gang,time,energy)
         self.pixel.AddPCD(x,y,z,time,energy)
@@ -142,25 +173,49 @@ class MainWindow(QtGui.QWidget):
     self.runButton = QtGui.QPushButton('start run',self)
     self.runButton.clicked.connect(self.doAction)
     self.fileWidget = FileWidget("Output file:",self)
-    self.fileWidget.move(0,100)
+    self.fileWidget.move(0,200)
     self.neventsWidget = NumberWidget("# events",self)
     self.neventsWidget.move(0,30)
     self.nsignalsWidget = NumberWidget("# signals",self)
     self.nsignalsWidget.setNumber(1)
     self.nsignalsWidget.move(120,30)
+    self.minenergyWidget = FloatWidget("min energy",self)
+    self.minenergyWidget.setNumber(100)
+    self.minenergyWidget.move(0,80)
+    self.maxenergyWidget = FloatWidget("max energy",self)
+    self.maxenergyWidget.setNumber(1000)
+    self.maxenergyWidget.move(120,80)
+    self.maxdtWidget = FloatWidget("max dt",self)
+    self.maxdtWidget.setNumber(10)
+    self.maxdtWidget.move(240,80)
+    self.minzWidget = FloatWidget("min z",self)
+    self.minzWidget.setNumber(-160)
+    self.minzWidget.move(0,130)
+    self.maxzWidget = FloatWidget("max z",self)
+    self.maxzWidget.setNumber(160)
+    self.maxzWidget.move(120,130)
+    self.maxrWidget = FloatWidget("max r",self)
+    self.maxrWidget.setNumber(160)
+    self.maxrWidget.move(240,130)
     self.noiseCheckbox = QtGui.QCheckBox("use real noise",self)
     self.noiseCheckbox.move(240,50)
     self.noiseFileWidget = FileWidget("Noise trace file:",self)
-    self.noiseFileWidget.move(0,150)
+    self.noiseFileWidget.move(0,250)
     self.dig = Digitization()
     self.progress = ProgressWidget(self)
-    self.progress.setGeometry(0,250,400,25)
+    self.progress.setGeometry(0,330,400,25)
 
     self.dig.neventsChanged.connect(self.progress.bar.setMaximum)
     self.dig.eventProcessed.connect(self.progress.bar.setValue)
     self.dig.finished.connect(self.progress.disable)
     self.dig.setNevents(self.neventsWidget.getNumber())
     self.dig.setNsignals(self.nsignalsWidget.getNumber())
+    self.dig.setMinEnergy(self.minenergyWidget.getNumber())
+    self.dig.setMaxEnergy(self.maxenergyWidget.getNumber())
+    self.dig.setMaxDt(self.maxdtWidget.getNumber())
+    self.dig.setMinZ(self.minzWidget.getNumber())
+    self.dig.setMaxZ(self.maxzWidget.getNumber())
+    self.dig.setMaxR(self.maxrWidget.getNumber())
     self.dig.setFilename(self.fileWidget.getFilename())
 
     self.progress.bar.setMinimum(0)
@@ -171,9 +226,15 @@ class MainWindow(QtGui.QWidget):
     self.noiseFileWidget.fileChanged.connect(self.dig.setNoiseFilename)
     self.neventsWidget.numberChanged.connect(self.dig.setNevents)
     self.nsignalsWidget.numberChanged.connect(self.dig.setNsignals)
+    self.minenergyWidget.numberChanged.connect(self.dig.setMinEnergy)
+    self.maxenergyWidget.numberChanged.connect(self.dig.setMaxEnergy)
+    self.maxdtWidget.numberChanged.connect(self.dig.setMaxDt)
+    self.minzWidget.numberChanged.connect(self.dig.setMinZ)
+    self.maxzWidget.numberChanged.connect(self.dig.setMaxZ)
+    self.maxrWidget.numberChanged.connect(self.dig.setMaxR)
     self.noiseCheckbox.stateChanged.connect(self.dig.setUseRealNoise)
 
-    self.setGeometry(400,400,400,300)
+    self.setGeometry(200,200,400,380)
     self.show()
     self.progress.hide()
 
@@ -231,6 +292,32 @@ class NumberWidget(QtGui.QWidget):
     super(NumberWidget,self).__init__(parent)
     self.numberEdit = QtGui.QSpinBox(self)
     self.numberEdit.setMaximum(10000000)
+    self.numberEdit.setValue(1000)
+    self.numberEdit.setGeometry(0,0,80,25)
+    self.numberEdit.valueChanged.connect(self.valueChanged)
+    self.numberEdit.move(0,25)
+
+    self.label = QtGui.QLabel(label,self)
+    self.label.setGeometry(0,0,80,25)
+
+  def valueChanged(self,number):
+    self.numberChanged.emit(number)
+
+  def getNumber(self):
+    return self.numberEdit.value()
+
+  def setNumber(self,number):
+    self.numberEdit.setValue(number)
+
+class FloatWidget(QtGui.QWidget):
+
+  numberChanged = QtCore.pyqtSignal(float)
+
+  def __init__(self,label,parent):
+    super(FloatWidget,self).__init__(parent)
+    self.numberEdit = QtGui.QDoubleSpinBox(self)
+    self.numberEdit.setMaximum(10000000)
+    self.numberEdit.setMinimum(-10000000)
     self.numberEdit.setValue(1000)
     self.numberEdit.setGeometry(0,0,80,25)
     self.numberEdit.valueChanged.connect(self.valueChanged)
