3bd6bb5e3b8117e210ce48304e084c237f6cc4a9
Wolfhart Feldmeier <wolfhart.feldmeier@mytum.de>
2012-10-16 15:32:21 +0200

    in testPositionPdf.py: added labels

--- test/testPositionPdf.py
+++ test/testPositionPdf.py
@@ -2,6 +2,7 @@ import ROOT,math
 
 def main():
   ROOT.gSystem.Load("libEXOROOT")
+  ROOT.gStyle.SetOptStat(0)
   cluster = ROOT.EXOClusteringModule()
   lowerx = -250
   upperx = 250
@@ -9,7 +10,9 @@ def main():
   uppery = 250
   nbinsx = upperx - lowerx
   nbinsy = uppery - lowery
-  hist = ROOT.TH2D("hist","hist",nbinsx,lowerx,upperx,nbinsy,lowery,uppery)
+  hist = ROOT.TH2D("hist","Distance from wire hexagon pdf",nbinsx,lowerx,upperx,nbinsy,lowery,uppery)
+  hist.GetXaxis().SetTitle("X (mm)")
+  hist.GetYaxis().SetTitle("Y (mm)")
   for i,x in enumerate(range(lowerx,upperx)):
     for j,y in enumerate(range(lowery,uppery)):
       coord = ROOT.EXOCoordinates(ROOT.EXOMiscUtil.kXYCoordinates,x,y,100.0,0.0)
