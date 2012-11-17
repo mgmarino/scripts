1f31af84b49b16407fc828087c59516e4c644e11
Wolfhart Feldmeier <wolfhart.feldmeier@mytum.de>
2012-11-17 10:39:13 +0100

    additional plots in plotUvsVTimeDiffNotClustered.py

--- Clustering/plotUvsVTimeDiffNotClustered.py
+++ Clustering/plotUvsVTimeDiffNotClustered.py
@@ -3,6 +3,8 @@ import ROOT,sys
 def main(filenames):
   ROOT.gSystem.Load("libEXOUtilities")
 
+  selection = set()
+
   hist = ROOT.TH1D("hist","hist",400,-20,20)
   hist.GetXaxis().SetTitle("U-time - V-time")
 
@@ -38,7 +40,22 @@ def main(filenames):
 
   hist10 = ROOT.TH1D("hist10","Z of high-U low-V clusters",200,-200,200)
   hist10.GetXaxis().SetTitle("Z")
+
+  hist11 = ROOT.TH2D("hist11","Energy of unclustered clusters in left peak",100,0,3000,100,0,800)
+  hist11.GetXaxis().SetTitle("Deposition Energy")
+  hist11.GetYaxis().SetTitle("Induction Energy")
   
+  hist12 = ROOT.TH2D("hist12","Energy of unclustered clusters in central peak",100,0,3000,100,0,800)
+  hist12.GetXaxis().SetTitle("Deposition Energy")
+  hist12.GetYaxis().SetTitle("Induction Energy")
+  
+  hist13 = ROOT.TH2D("hist13","Energy of unclustered clusters in right peak",100,0,3000,100,0,800)
+  hist13.GetXaxis().SetTitle("Deposition Energy")
+  hist13.GetYaxis().SetTitle("Induction Energy")
+  
+  hist14 = ROOT.TH1D("hist14","Time diff to nearest U-cluster",400,-20,20)
+  hist14.GetXaxis().SetTitle("time")
+
   t = ROOT.TChain("tree")
   for f in filenames:
     t.Add(f)
@@ -67,6 +84,15 @@ def main(filenames):
           hist7.Fill(timediff)
 
     for uc in UonlyClusters:
+      mindiff = 1000000.
+      for j in range(ED.GetNumChargeClusters()):
+        other = ED.GetChargeCluster(j)
+        if other == uc:
+          continue
+        diff = (uc.fCollectionTime - other.fCollectionTime) / 1000
+        if abs(diff) < abs(mindiff):
+          mindiff = diff
+      hist14.Fill(mindiff)
       for vc in VonlyClusters:
         timediff = (uc.fCollectionTime - vc.fCollectionTime) / 1000
         hist.Fill(timediff)
@@ -85,8 +111,15 @@ def main(filenames):
           if abs(uc.fZ) < 160:
             hist9.Fill(timediff)
           hist10.Fill(uc.fZ)
-        if timediff < -2. and timediff > -4.:
+        if timediff > -5. and timediff < 5:
           print(ED.fEventNumber)
+          selection.add(i)
+        if timediff > -2.6 and timediff < -2.3:
+          hist11.Fill(uc.fCorrectedEnergy,vc.fCorrectedAmplitudeInVChannels)
+        elif timediff > -1 and timediff < 0.4:
+          hist12.Fill(uc.fCorrectedEnergy,vc.fCorrectedAmplitudeInVChannels)
+        elif timediff > 1.6 and timediff < 3.5:
+          hist13.Fill(uc.fCorrectedEnergy,vc.fCorrectedAmplitudeInVChannels)
 
   canvas1 = ROOT.TCanvas("canvas1")
   hist.Draw()
@@ -108,7 +141,31 @@ def main(filenames):
   hist9.Draw()
   canvas10 = ROOT.TCanvas("canvas10")
   hist10.Draw()
-  raw_input("hit enter to quit")
+  canvas11 = ROOT.TCanvas("canvas11")
+  hist11.Draw("colz")
+  canvas12 = ROOT.TCanvas("canvas12")
+  hist12.Draw("colz")
+  canvas13 = ROOT.TCanvas("canvas13")
+  hist13.Draw("colz")
+  canvas14 = ROOT.TCanvas("canvas14")
+  hist14.Draw()
+  answer = ""
+  while answer not in (["Y","y","N","n"]):
+    answer = raw_input(str(len(selection))+"/"+str(t.GetEntries())+" events selected. Do you want to save the selected events? (Y/N) ")
+  if answer in ["Y","y"]:
+    infile = raw_input("Please enter waveform data file: ")
+    outfile = raw_input("Please enter output filename: ")
+    SaveSelection(selection,infile,outfile)
+
+def SaveSelection(selection,infile,outfile):
+  entries = ROOT.TEntryList()
+  map(entries.Enter,selection)
+  f = ROOT.TFile(infile)
+  t = f.Get("tree")
+  t.SetEntryList(entries)
+  out = ROOT.TFile(outfile,"RECREATE")
+  new_tree = t.CopyTree("")
+  new_tree.Write()
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
