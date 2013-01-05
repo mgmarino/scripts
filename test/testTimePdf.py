98a2ed1b5a76ecfe9b37d00889f83cf87ceb3c80
Wolfhart Feldmeier <wolfhart.feldmeier@mytum.de>
2013-01-05 09:50:18 +0100

    changes in testTimePdf.py

--- test/testTimePdf.py
+++ test/testTimePdf.py
@@ -2,19 +2,27 @@ import ROOT,math
 
 def main():
   ROOT.gSystem.Load("libEXOROOT")
+  ROOT.gStyle.SetOptStat(0)
   cluster = ROOT.EXOClusteringModule()
-  lowert = -5
-  uppert = 5
-  nbins = (uppert - lowert) * 10
-  hist = ROOT.TH1D("hist","hist",nbins,lowert,uppert)
-  for i in range(nbins):
-    time = (lowert + float(i)/10.)*1000.
-    #val = math.exp(-cluster.timeNLPdf(0.,time))
-    val = cluster.timeNLPdf(0.,time)
-    print("val(t = " + str(time/1000.) + ") = " + str(val))
-    hist.SetBinContent(i,val)
+  minT = -5000
+  maxT = 5000
+  stepT = 100
+  minZ = 0
+  maxZ = 200
+  stepZ = 1
+  Tbins = range(minT,maxT,stepT)
+  Zbins = range(minZ,maxZ,stepZ)
+  hist = ROOT.TH2D("hist","hist",len(Zbins),minZ,maxZ,len(Tbins),minT/1000.,maxT/1000.)
+  hist.SetTitle("U-V time difference pdf")
+  hist.GetXaxis().SetTitle("Z (mm)")
+  hist.GetYaxis().SetTitle("#DeltaT (#mus)")
+  for x,z in enumerate(Zbins):
+    for y,t in enumerate(Tbins):
+      val = cluster.timeNLPdf(0.,t,z,False)
+      print("val(t = " + str(t/1000.) + ", z = " + str(z) +") = " + str(val))
+      hist.SetBinContent(x,y,val)
   c1 = ROOT.TCanvas()
-  hist.Draw()
+  hist.Draw("colz")
   raw_input("hit enter to quit")
 
 if __name__ == "__main__":
