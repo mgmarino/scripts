a66e7003292319e7c253b0c02bebc5f0b3e672e3
Wolfhart Feldmeier <wolfhart.feldmeier@googlemail.com>
2013-05-13 03:49:31 -0700

    modified GenerateAPDsumNoiseTraces. Can take more than one input file

--- Digitization/GenerateAPDsumNoiseTraces.py
+++ Digitization/GenerateAPDsumNoiseTraces.py
@@ -1,53 +1,57 @@
 import ROOT,sys,math
 
-if len(sys.argv) != 2:
-  print("usage: "+sys.argv[0]+" LowBGDataFile")
-  sys.exit(1)
+def main(t)
+  ED = ROOT.EXOEventData()
+  t.SetBranchAddress("EventBranch",ED)
+  t.GetEntry(0)
 
-ROOT.gSystem.Load("libEXOROOT")
-f = ROOT.TFile(sys.argv[1])
-t = f.Get("tree")
+  output = ROOT.TFile("NoiseTraces"+str(t.EventBranch.fRunNumber)+".root","RECREATE")
+  outtree = ROOT.TTree("TraceTree","TraceTree")
+  wfNorth = ROOT.EXODoubleWaveform()
+  wfNorth.SetLength(2048)
+  wfSouth = ROOT.EXODoubleWaveform()
+  wfSouth.SetLength(2048)
+  outtree.Branch("wfNorth","EXODoubleWaveform",wfNorth)
+  outtree.Branch("wfSouth","EXODoubleWaveform",wfSouth)
 
-ED = ROOT.EXOEventData()
-t.SetBranchAddress("EventBranch",ED)
-t.GetEntry(0)
+  counter = 0
 
-output = ROOT.TFile("NoiseTraces"+str(t.EventBranch.fRunNumber)+".root","RECREATE")
-outtree = ROOT.TTree("TraceTree","TraceTree")
-wfNorth = ROOT.EXODoubleWaveform()
-wfNorth.SetLength(2048)
-wfSouth = ROOT.EXODoubleWaveform()
-wfSouth.SetLength(2048)
-outtree.Branch("wfNorth","EXODoubleWaveform",wfNorth)
-outtree.Branch("wfSouth","EXODoubleWaveform",wfSouth)
+  n = t.GetEntries()
+  for i in range(n):
+    if not i%(n/100):
+      print("processing entry "+str(i)+" ("+str(float(i)/n*100.)+"%)")
+    t.GetEntry(i)
+    if ED.fEventHeader.fTaggedAsNoise or ED.fEventHeader.fTaggedAsMuon:
+      continue
+    if ED.fEventHeader.fSumTriggerRequest or ED.fEventHeader.fIndividualTriggerRequest:
+      continue
+    wfd = ED.GetWaveformData()
+    wfd.Decompress()
+    wfNorth.Zero()
+    wfSouth.Zero()
+    for j in range(1,37):
+      wf1 = wfd.GetWaveformWithChannel(152+j).Convert()
+      if wf1.GetLength() != 2048: continue
+      wf2 = wfd.GetWaveformWithChannel(189+j).Convert()
+      if wf2.GetLength() != 2048: continue
+      if counter == 0:
+        wfNorth.SetSamplingFreq(wf1.GetSamplingFreq())
+        wfSouth.SetSamplingFreq(wf2.GetSamplingFreq())
+      wfNorth += wf1
+      wfSouth += wf2
+    counter += 1
+    outtree.Fill()
 
-counter = 0
+  outtree.Write()
+  output.Close()
 
-n = t.GetEntries()
-for i in range(n):
-  if not i%(n/100):
-    print("processing entry "+str(i)+" ("+str(float(i)/n*100.)+"%)")
-  t.GetEntry(i)
-  if ED.fEventHeader.fTaggedAsNoise or ED.fEventHeader.fTaggedAsMuon:
-    continue
-  if ED.fEventHeader.fSumTriggerRequest or ED.fEventHeader.fIndividualTriggerRequest:
-    continue
-  wfd = ED.GetWaveformData()
-  wfd.Decompress()
-  wfNorth.Zero()
-  wfSouth.Zero()
-  for j in range(1,37):
-    wf1 = wfd.GetWaveformWithChannel(152+j).Convert()
-    if wf1.GetLength() != 2048: continue
-    wf2 = wfd.GetWaveformWithChannel(189+j).Convert()
-    if wf2.GetLength() != 2048: continue
-    if counter == 0:
-      wfNorth.SetSamplingFreq(wf1.GetSamplingFreq())
-      wfSouth.SetSamplingFreq(wf2.GetSamplingFreq())
-    wfNorth += wf1
-    wfSouth += wf2
-  counter += 1
-  outtree.Fill()
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print("usage: "+sys.argv[0]+" LowBGDataFile[s]")
+    sys.exit(1)
 
-outtree.Write()
-output.Close()
+  ROOT.gSystem.Load("libEXOROOT")
+  chain = ROOT.TChain("tree")
+  for f in sys.argv[1:]:
+    chain.add(f)
+  main(chain)
